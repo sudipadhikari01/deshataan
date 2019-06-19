7c1464fd23b56e65a0a3caad587d87d7514710ed
Susant Paudel <crupee.susant@gmail.com>
2019-06-19 10:46:27 +0545

    frontend PageController created and defined methods"

---
+++ app/Http/Controllers/FrontEnd/PageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\FrontEnd;
+
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller
+{
+
+    public function home()
+    {
+
+    }
+
+    public function packages()
+    {
+
+    }
+
+    public function hotels()
+    {
+
+    }
+
+    public function contact()
+    {
+
+    }
+
+}
