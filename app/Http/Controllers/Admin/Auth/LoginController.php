f610e49c2be1d053eabdfc3f069bf6f3738649ea
Susant Paudel <crupee.susant@gmail.com>
2019-07-03 11:15:13 +0545

    some bug resolved

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Admin\Auth;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class LoginController extends Controller
 {
     //
+    public function showLoginForm()
+    {
+
+    }
 }
